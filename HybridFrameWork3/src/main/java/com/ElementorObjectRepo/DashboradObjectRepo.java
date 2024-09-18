9175b85326f9e471a59cf37f82220db6df717784
Hemant3234 <168821754+Hemant3234@users.noreply.github.com>
2024-09-18 13:14:44 +0530

    Update DashboradObjectRepo.java

--- HybridFrameWork3/src/main/java/com/ElementorObjectRepo/DashboradObjectRepo.java
+++ HybridFrameWork3/src/main/java/com/ElementorObjectRepo/DashboradObjectRepo.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 
 public class DashboradObjectRepo {
 	
-	@FindBy(xpath="//b[text()='Java By Kiran']")
+	@FindBy(xpath="//b[text()='jbk']")
 	public WebElement headingDashboard;
 	@FindBy(xpath="//span[text()='Users']")
 	public WebElement userbutton;
